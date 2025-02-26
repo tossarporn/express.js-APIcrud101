e3e61dcb3c14f078acaaa751109f31178f1f9cee
tossarporn supanikorn <tossarporn@gmail.com>
2025-02-26 17:12:46 +0700

    add controller update,remove and routes

--- Controllers/product.js
+++ Controllers/product.js
@@ -12,3 +12,33 @@ exports.list = async(req,res)=>{
         res.status(500).send('not list')
     }
 }
+
+exports.create = async (req,res)=>{
+    try{
+        res.send('Hello create');
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send('not create')
+    }
+}
+
+exports.update = async(req,res)=>{
+    try{
+        res.send('Hello update')
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send('not update')
+    }
+}
+
+exports.remove = async(req,res)=>{
+    try{
+        res.send('Hello delete')
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send('not delete')
+    }
+}
--- Routes/product.js
+++ Routes/product.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { read ,list} = require('../Controllers/product')
-
+const { read ,list,create,update,remove} = require('../Controllers/product')
+ 
 router.get('/product',list)
 
 router.get('/product/:id',read)
 
-router.post('/product',(req,res)=>{
-    res.send('Hello Post product ')
-})
+router.post('/product',create)
 
-router.put('/product/:id',(req,res)=>{
-    res.send('Hello Put product')
-})
+router.put('/product/:id',update)
 
-router.delete('/product/:id',(req,res)=>{
-    res.json({name: 'phobia',id:1})
-})
+router.delete('/product/:id',remove)
 
 module.exports = router
