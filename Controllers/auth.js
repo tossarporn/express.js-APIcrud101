f33f46360856a8fada3d0ba083cd1e653fdc8db6
tossarporn supanikorn <tossarporn@gmail.com>
2025-02-27 13:50:49 +0700

    add method post for login from Routes and Controller

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -9,3 +9,12 @@ exports.register = async(req,res)=>{
         res.status(500).send('Server Error')
     }
 }
+
+exports.login = async(req,res)=>{
+    try{
+        res.send('Hello Login Controller')
+    }
+    catch(err){
+        console.log(err)
+    }
+}
--- Routes/auth.js
+++ Routes/auth.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { register } = require('../Controllers/auth')
+const { register,login } = require('../Controllers/auth')
 router.post('/register',register)
 
-router.post('/login',(req,res)=>{
-    res.send('Hello login')
-})
+router.post('/login',login)
 
 module.exports = router
