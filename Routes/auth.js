f5da7c9fce53b55fca57b5b5854d7aac372ae340
tossarporn supanikorn <tossarporn@gmail.com>
2025-02-27 13:30:14 +0700

    add method post for auth

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/auth',(req,res)=>{
-    res.send('Hello auth')
+router.post('/register',(req,res)=>{
+    res.send('Hello register')
+})
+
+router.post('/login',(req,res)=>{
+    res.send('Hello login')
 })
 
 module.exports = router
