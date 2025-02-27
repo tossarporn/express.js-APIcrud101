76199224eeb28c28eecf98d0f0b7dba922049ff8
tossarporn supanikorn <tossarporn@gmail.com>
2025-02-27 14:10:54 +0700

    CheckUser from mongooseDB

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,8 +1,23 @@
+const User = require('../Models/User');
 
 
 exports.register = async(req,res)=>{
     try{
-        res.send('Hello Register Controller')
+        //1. checkuser
+        const {name,password} = req.body
+        const user = await User.findOne({name})
+        console.log(user)
+
+        if(user){
+            return res.send('User Already Exists!').status(400)
+        }
+
+        //2. encrypt
+        
+        //3. save data 
+
+
+        res.send(req.body)
     }
     catch(err){
         console.log(err)
